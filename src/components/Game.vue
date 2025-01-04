9b3a61b937144bf2763f909196fcfe5be3633474
usuless <61857692+usuless@users.noreply.github.com>
2025-01-04 17:19:42 +0100

    timer problem

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { reactive, ref} from 'vue';
-import { setTimer } from '../utilities/timer';
 import type { Ref } from 'vue';
-import Timer from './Timer.vue';
 import UIFx from 'uifx';
 import { handleKey } from '../utilities/keyHandler';
 import { findTarget } from '../utilities/findTarget';
@@ -26,13 +24,11 @@ let isGameOn: Ref<boolean> = ref(false)
 let wallpaper = reactive({
   class: "bg-black opacity-35"
 })
-let timer: Ref<any> = ref(0)
 let moves: string[] = ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"]
 
 document.addEventListener('keydown', function(e) {
   if(isGameOn.value === false && moves.includes(e.key)) {
     isGameOn.value = true
-    timer.value = setTimer(15)
     wallpaper.class = ""
   }
       parsedGame.value = handleKey(e.key, parsedGame.value, playerLocation)    
@@ -48,11 +44,11 @@ document.addEventListener('keydown', function(e) {
 
 parsedGame.value = placePoint(parsedGame.value)
 let pointLocation = findTarget(parsedGame.value, "*")
+
 </script>
 
 <template>
   <div class="flex flex-col h-full pt-10 items-center">
-    <Timer v-model:time="timer.seconds" v-model:is-game-on="isGameOn"/>
       <div v-bind="wallpaper" class="flex justify-around w-2/12" v-for="line in parsedGame">
         <div class="" v-for="letter in line">
           <p class="size-4"  v-bind="fieldCheck(letter)">
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createApp } from 'vue';
+import VueCountdown from '@chenfengyuan/vue-countdown';
 
+const app = createApp({});
+
+app.component(VueCountdown.name, VueCountdown);
 createApp(App).mount('#app')
