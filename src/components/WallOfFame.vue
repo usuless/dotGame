e649489183cc1d20a42ef50c6fd66f31e0855e43
usuless <61857692+usuless@users.noreply.github.com>
2025-02-20 15:03:34 +0100

    npm

--- src/components/WallOfFame.vue
+++ src/components/WallOfFame.vue
@@ -1,10 +1,53 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+import { useSorted } from '@vueuse/core';
+
 const {data , points} = defineProps<{
-    data: Object,
+    data: Array<any>,
     points: number
 }>()
+
+let previousScores: Ref<Array<number>> = ref([])
+
+let finallySorted: Array<number> = []
+
+const isScoreOnBoard = () => {
+        previousScores.value.push(points)
+        //@ts-ignore
+        let finalSort = useSorted(previousScores.value)
+        finallySorted = sorting(finalSort.value)
+}
+
+const getIdArray = () => {
+
+    data.forEach(player => {
+        previousScores.value.push(player.score)
+    })
+}
+
+const sorting = (array: Array<number>) => {
+    let a1 = []
+    array.forEach(element => {
+    if (!a1.includes(element)) {
+        a1.push(element);
+    }
+});
+return a1
+}
+
+onMounted(() => {
+    getIdArray()
+    isScoreOnBoard()
+})
+
 </script>
 <template>
-    {{ data }}
-    {{ points }}
+    <div class="flex items-center justify-center">
+        <div class="w-6/12">
+            {{ finallySorted }}
+            {{ data }}
+        </div>
+    </div>
 </template>
